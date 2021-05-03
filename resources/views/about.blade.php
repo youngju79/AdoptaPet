6bde61ffe17dce30344025af4175b6c947a88c88
Yong Zu Shin <yongzush@usc.edu>
2021-05-02 22:05:30 -0400

    edited README.md

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -18,7 +18,7 @@
         <h2 class="text-center">About Us</h2>
         <p class="fw-bold fs-5">What is the goal/mission of this application?</p>
         <p class="fs-5">
-            The goal of this web application is to simplify the process of adopting/listing a pet for adoption. Using a simple and intuitive design, 
+            The goal of this web application is to simplify the process of adopting/listing a pet for adoption. With a simple and intuitive design, 
             users are able to easily find a pet to adopt or list a pet for adoption. Users are able to view a list of pets that are up for adoption 
             along with the contact information of the pets' owners. All of the pets' information (breed type, age, etc.) will also be displayed. Registered 
             users of the application will be able to put their pets up for adoption, while unregistered users will only be able to view a list of pets that 
@@ -27,7 +27,7 @@
         <p class="fw-bold fs-5">Who is the primary audience?</p>
         <p class="fs-5">
             The primary audience of this web application ranges from young to older aged adults who are looking to adopt 
-            a pet or put a pet up for adoption. Users who are looking to adopt a pet on this website will most likely be part 
+            a pet or list a pet for adoption. Users who are looking to adopt a pet on this website will most likely be part 
             of the younger-middle generation who are looking to start a new family. 
         </p>
     </div>
